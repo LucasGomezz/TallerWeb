d755c713380712a981f284eb16f8dbe517faac4b
Usuario <pablot.pavan@gmail.com>
2023-09-22 15:52:36 -0300

    vero

---
+++ src/main/java/com/tallerwebi/presentacion/ControladorPartida.java
@@ -0,0 +1,20 @@
+package com.tallerwebi.presentacion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorPartida {
+        @RequestMapping("/nueva-partida")
+        public ModelAndView irANuevaPartida() {
+
+            return new ModelAndView("partido");
+        }
+        @RequestMapping("/partido")
+        public ModelAndView irAPartido() {
+
+            return new ModelAndView("partido");
+        }
+    }
+
--- src/main/java/com/tallerwebi/presentacion/ControladorTienda.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorTienda.java
@@ -1,14 +1,18 @@
 package com.tallerwebi.presentacion;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ControladorTienda {
-    @RequestMapping("/tienda")
+    @RequestMapping("/home")
     public ModelAndView irATienda() {
 
         return new ModelAndView("tienda");
+    }
+    @RequestMapping("/partida")
+    public ModelAndView irANuevaPartida() {
+
+        return new ModelAndView("nueva-partida");
     }
 }
