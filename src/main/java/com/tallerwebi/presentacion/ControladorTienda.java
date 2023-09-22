3145ffcf1d32f49725e936fa76ed4ccecddf9b2b
Usuario <pablot.pavan@gmail.com>
2023-09-22 15:59:32 -0300

    arreglo

--- src/main/java/com/tallerwebi/presentacion/ControladorTienda.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorTienda.java
@@ -5,14 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ControladorTienda {
-    @RequestMapping("/home")
+    @RequestMapping("/tienda")
     public ModelAndView irATienda() {
 
         return new ModelAndView("tienda");
     }
-    @RequestMapping("/partida")
-    public ModelAndView irANuevaPartida() {
 
-        return new ModelAndView("nueva-partida");
-    }
 }
