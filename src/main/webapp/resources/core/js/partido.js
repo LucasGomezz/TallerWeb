95129ecd94452aa742c582990cf7659987d27ff7
Usuario <pablot.pavan@gmail.com>
2023-10-13 08:57:29 -0300

    js

---
+++ src/main/webapp/resources/core/js/partido.js
@@ -0,0 +1,15 @@
+const botonDriblear = document.querySelector('#driblear');
+const botonTirar = document.querySelector('#tirar');
+const botonPasar = document.querySelector('#pasar');
+
+    botonDriblear.addEventListener('click', () => {
+
+    });
+
+    botonTirar.addEventListener('click', () => {
+
+    });
+
+    botonPasar.addEventListener('click', () => {
+
+    });
